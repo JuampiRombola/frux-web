769456eab20ca5f5fbb2dcf201bf8606b4fe4f04
JuampiRombola <juampi.rombola@gmail.com>
2021-05-01 11:34:34 -0300

    Add vue-apollo without examples nor graphql server

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  pluginOptions: {
+    // Apollo-related options
+    apollo: {
+      // Enable ESLint for `.gql` files
+      lintGQL: true
+    }
+  }
 }
