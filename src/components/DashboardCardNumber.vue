476249f3a6c63afac5c6d3ea229af2593ee296b8
JuampiRombola <juampi.rombola@gmail.com>
2021-05-12 20:29:40 -0300

    Add Dashboard components

---
+++ src/components/DashboardCardNumber.vue
@@ -0,0 +1,80 @@
+<template>
+  <v-card class="mt-7 mx-auto">
+    <v-row>
+      <v-col class="text-center mb-0 pb-0" cols="4">
+        <v-sheet
+          class="v-sheet--offset ml-3"
+          height="88"
+          width="88"
+          rounded
+          :color="color"
+          elevation="12"
+          max-width="auto"
+        >
+          <v-container fill-height fluid>
+            <v-row align="center" justify="center">
+              <v-col class="text-center">
+                <v-icon x-large color="white">{{ icon }}</v-icon>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-sheet>
+      </v-col>
+      <v-col cols="8" class="mb-0 pb-0">
+        <v-card-text class="pt-0">
+          <div class="title font-weight-light mb-2 text-center text-no-wrap">
+            {{ title }}
+          </div>
+          <div class="headline font-weight-medium grey--text text-center">
+            {{ quantity }}
+          </div>
+        </v-card-text>
+      </v-col>
+      <v-col cols="12" class="mt-0 pt-0">
+        <v-card-text class="py-0">
+          <v-divider class="my-2"></v-divider>
+          <v-icon
+            class="mr-2"
+            small
+          >
+            mdi-clock
+          </v-icon>
+          <span class="caption grey--text font-weight-light">{{ captionText }}</span>
+        </v-card-text>
+      </v-col>
+    </v-row>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'DashboardCardNumber',
+
+  props: {
+    title: {
+      type: String
+    },
+    quantity: {
+      type: String
+    },
+    icon: {
+      type: String
+    },
+    captionText: {
+      type: String
+    },
+    color: {
+      type: String
+    }
+  },
+
+  data: () => ({})
+}
+</script>
+
+<style scoped>
+.v-sheet--offset {
+  top: -20px;
+  position: relative;
+}
+</style>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,10 +1,38 @@
 <template>
-  <h1>Dashboard {{ msg }} </h1>
+  <v-container>
+    <v-row>
+      <v-col cols="3">
+        <DashboardCardNumber title="Online" quantity="1344" icon="mdi-account-group" captionText="En tiempo real" color="#18aa5e"></DashboardCardNumber>
+      </v-col>
+      <v-col cols="3">
+        <DashboardCardNumber title="Veedurías" quantity="25" icon="mdi-account-group" captionText="Últimas 24 horas" color="#18aa5e"></DashboardCardNumber>
+      </v-col>
+      <v-col cols="3">
+        <DashboardCardNumber title="Patrocinios" quantity="87" icon="mdi-account-group" captionText="Últimas 24 horas" color="#18aa5e"></DashboardCardNumber>
+      </v-col>
+      <v-col cols="3">
+        <DashboardCardNumber title="Transacciones" quantity="106" icon="mdi-account-group" captionText="Últimas 24 horas" color="#18aa5e"></DashboardCardNumber>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="6">
+        <DashboardUserRegistrations></DashboardUserRegistrations>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
+import DashboardUserRegistrations from '@/components/DashboardUserRegistrations.vue'
+import DashboardCardNumber from '@/components/DashboardCardNumber.vue'
+
 export default {
   name: 'Dashboard',
+
+  components: {
+    DashboardUserRegistrations,
+    DashboardCardNumber
+  },
 
   props: {
     msg: String
