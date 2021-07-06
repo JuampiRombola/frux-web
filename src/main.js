2cfaeff8c37b32c3a01e3133658e08978a30fc50
Juampi Rombolá <juampi.rombola@gmail.com>
2021-07-05 22:14:12 -0300

    Remove vue-scrollbar

--- src/main.js
+++ src/main.js
@@ -5,14 +5,9 @@ import store from './store'
 import vuetify from './plugins/vuetify'
 import { createProvider } from './vue-apollo'
 import VueLayers from 'vuelayers'
-import PerfectScrollbar from 'vue2-perfect-scrollbar'
-import 'vue2-perfect-scrollbar/dist/vue2-perfect-scrollbar.css'
-
 Vue.use(VueLayers, {
   dataProjection: 'EPSG:4326'
 })
-
-Vue.use(PerfectScrollbar)
 
 Vue.config.productionTip = false
 
