f6cc76846aa7e2b3e2816799d3c94078a529fc0f
Juampi Rombolá <juampi.rombola@gmail.com>
2021-07-28 20:42:37 -0300

    Fix metrics for firefox

--- src/views/Metrics.vue
+++ src/views/Metrics.vue
@@ -7,7 +7,7 @@
         </v-col>
       </v-row>
     </v-container>
-    <div class="iframe-content" v-show="!loading">
+    <div class="iframe-content">
       <iframe
         src="https://p.datadoghq.com/sb/1f4ff3ee-bcb9-11eb-a478-da7ad0900002-cdf8903be7f6f7d0787e3a6d4dfe8807"
         width="80%"
@@ -38,7 +38,7 @@ iframe {
   display: block;
   border: none;         /* Reset default border */
   height: 100vh;        /* Viewport-relative units */
-  width: calc(100% + 17px);
+  width: calc(100% + 15px);
 }
 .iframe-content {
   width: 100%;
